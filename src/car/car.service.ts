b81f6e8225706d8aaf1dbdbc2b937bffed3dbbfa
ighmaz <ighmazcool@gmail.com>
2023-02-21 00:04:24 +0530

    fix stuff

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -1,37 +1,13 @@
 import { HttpException, Injectable } from '@nestjs/common';
-import { Cars } from './cars.mock';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Icar } from './interfaces/car.interface';
+import { CarDto } from './car.dto';
 @Injectable()
 export class CarService {
-  private cars = Cars; // from the mock car
+  public getCars() {}
 
-  public getCars() {
-    return this.cars;
-  }
-
-  public postCar(car) {
-    return this.cars.push(car);
-  }
-  public getCarById(id: number): Promise<any> {
-    const carId = Number(id);
-    return new Promise((resolve) => {
-      const car = this.cars.find((car) => car.id === carId);
-
-      if (!car) {
-        // throw an error if not found
-        throw new HttpException('could not find ', 404);
-      }
-      return resolve(car);
-    });
-  }
-  public deleteCarById(id: number): Promise<any> {
-    const carId = Number(id);
-    return new Promise((resolve) => {
-      const index = this.cars.findIndex((car) => car.id === carId);
-      if (index === -1) {
-        throw new HttpException('could not find ', 404);
-      }
-      this.cars.splice(index, 1);
-      return resolve(this.cars);
-    });
-  }
+  public postCar(car) {}
+  public getCarById(id: number): Promise<any> {}
+  public deleteCarById(id: number): Promise<any> {}
 }
